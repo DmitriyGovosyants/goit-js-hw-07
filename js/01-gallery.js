ca2fe78a88c560deae2b5f20bb941b00f6633802
Yelyzaveta Potekhina <potekhinaea@gmail.com>
2022-04-06 20:35:43 +0300

    add gallery1 markup and library

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,35 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryEl = document.querySelector('div.gallery');
 
-console.log(galleryItems);
+galleryEl.addEventListener('click', onBigImgOpen);
+
+function onBigImgOpen(evt) {
+    if (evt.target.nodeName !== 'IMG') {
+        return;
+    }
+
+    evt.preventDefault();
+
+    basicLightbox
+        .create(`
+            <img src="${evt.target.dataset.source}" alt="${evt.target.alt}">
+        `)
+        .show();
+};
+
+function makeGalleryMarkup() {
+    const markup = galleryItems
+    .map(({preview, original, description}) => 
+        `<div class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img class="gallery__image" src="${preview}" alt="${description}" data-source="${original}">
+            </a>
+        </div>`
+    )
+    .join('');
+
+    galleryEl.innerHTML = markup;
+};
+
+makeGalleryMarkup();
